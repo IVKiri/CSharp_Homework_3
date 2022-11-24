552bf605120248340d519d5f8730ebb4bd3661b7
Inna Kirianova <innr@ya.ru>
2022-11-24 23:42:16 +0300

    added code for task 19

---
+++ Task19/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+// является ли оно палиндромом.
+
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Write("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string num = Convert.ToString(number);
+
+if (num.Length != 5) {
+    Console.WriteLine("Это не пятизначное число. Попробуйе еще раз.");
+}
+else {
+    if (num[0] == num[4] && num[1] == num [3]) {
+        Console.WriteLine("Это палиндром");
+    }
+    else {
+        Console.WriteLine("Число не является палиндромом");
+    }
+}
